03fa6f5db12efeb54084e44fbf8fe4cc8ff6eae8
Alley Manalio <amanalio@ufl.edu>
2016-07-28 20:15:48 -0400

    Add visuallyhidden elements for screen readers

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-//To make the accordion work, it adds the class "show" when the h2 heading is clicked
+//To make the accordion work, it adds the class "show" when the h3 heading is clicked
 
-var accordion = document.getElementsByClassName('accordion');
-var i;
+$(document).ready(function () {
+    var accordion = $('.accordion');
+    var i;
 
-for (i = 0; i < accordion.length; i++) {
-    accordion[i].onclick = function () {
-        this.classList.toggle('active');
-        this.nextElementSibling.classList.toggle('show');
-    };
-}
+    for (i = 0; i < accordion.length; i++) {
+        accordion[i].onclick = function () {
+            this.classList.toggle('active');
+            this.nextElementSibling.classList.toggle('show');
+        };
+    }
+});
 
 //jQuery plugin code for Simple Text Rotator
 $(document).ready(function () {
